318fd684b6db718db4ba55720d55cd90846430b2
zZang32 <rbangelo1103@gmail.com>
2022-04-24 05:13:03 +0200

    db
    actualicé los tipos de las columnas, añadiendo el auto incremento y el valor no nulo.

--- db.sql
+++ db.sql
@@ -2,9 +2,9 @@ CREATE DATABASE `basic-api`;
 USE `basic-api`;
 CREATE TABLE events (
 
-    id int(11) primary key,
-    title varchar(255),
-    content text,
+    id int(11) primary key not null AUTO_INCREMENT,
+    title varchar(255) not null,
+    content text not null,
     date datetime
 
 );
